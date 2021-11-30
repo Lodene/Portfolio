f585418caf15ae2455404b7c808d7eacafa5ff77
Antoine <antoine.adjamidis@gmail.com>
2021-11-30 15:39:11 +0100

    ajout des pages qui viennent celon le bouton

--- js/cacher.js
+++ js/cacher.js
@@ -1,8 +1,10 @@
+
+let verif = 0;
+
 function onestla(element1, element2){
     let elabout = document.getElementById(element1);
     let eltexteabout = document.getElementById(element2);
-    console.log(elabout);
-    console.log(eltexteabout);
+    element1.onclick = bouger;
 
     elabout.addEventListener("mouseover", function( event ) {
         eltexteabout.style.opacity= "75%";
@@ -16,11 +18,23 @@ function onestla(element1, element2){
 
 }
 
-function bouger(element1){
-    let div = document.getElementById(element1);
-    element1.style.top = "10vh";
-    element1.
+function verification(val, element){
+    let fenetre = document.getElementById(element);
+    if (val == 1){
+        if (verif == 0){
+            verif ++;
+            fenetre.classList.add("showed");
+        } else {
+            sauvegarde.classList.remove("showed");
+            fenetre.classList.add("showed"); 
+        }
+    } else if (val == 2) {
+        fenetre.classList.remove("showed");
+        verif --;
+    }
+    sauvegarde = fenetre;
 }
+
 
 onestla("about", "texteabout");
 onestla("skills", "texteskills");
@@ -29,7 +43,5 @@ onestla("experience", "texteexperience");
 onestla("cv", "textecv");
 onestla("contact", "textecontact");
 
-onestla("linkedin", "textelinkedin");
-onestla("twitter", "textetwitter");
+onestla("reseaux", "textereseaux");
 
-bouger("uwu");
